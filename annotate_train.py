48f8450469adb620f48355fd3c3724aa247155ef
sThalham <thalhammer@acin.tuwien.ac.at>
2021-02-06 18:46:22 +0100

    debugging

--- annotate_train.py
+++ annotate_train.py
@@ -7,6 +7,21 @@ import csv
 import cv2
 
 
+fxkin = 572.41140
+fykin = 573.57043
+cxkin = 325.26110
+cykin = 242.04899
+
+
+def toPix_array(translation):
+
+    xpix = ((translation[:, 0] * fxkin) / translation[:, 2]) + cxkin
+    ypix = ((translation[:, 1] * fykin) / translation[:, 2]) + cykin
+    #zpix = translation[2] * fxkin
+
+    return np.stack((xpix, ypix), axis=1) #, zpix]
+
+
 def draw_axis(img, poses):
     # unit is mm
     points = np.float32([[100, 0, 0], [0, 100, 0], [0, 0, 100], [0, 0, 0]]).reshape(-1, 3)
@@ -34,6 +49,16 @@ if __name__ == "__main__":
     rgb_path = os.path.join(train_dir, 'rgb')
     #depth_path = os.path.join(train_dir, 'depth')
 
+    tdbox_ori = np.array([[0.25, 0.0, 0.1],  # [35.087, 35.787, 60.686]
+                                [0.25, 0.0, -0.05],
+                                [0.25, -0.25, -0.05],
+                                [0.25, -0.25, 0.1],
+                                [-0.0, 0.0, 0.1],
+                                [-0.0, 0.0, -0.05],
+                                [-0.0, -0.25, -0.05],
+                                [-0.0, -0.25, 0.1]])
+
+
     with open(associations_source, 'r') as f:
         reader = csv.reader(f, delimiter=" ")
         images = list(reader)
@@ -48,22 +73,30 @@ if __name__ == "__main__":
     #dep_imgs = os.listdir(depth_path)
 
     for idx in range(mar_source.shape[0]):
+
         img_path = os.path.join(".", images[idx][2][:-4])
         depth_img = cv2.imread(os.path.join(train_dir, images[idx][2]), -1)
-        depth_img = cv2.resize(depth_img, (640, 480))
+        img = cv2.resize(depth_img, (640, 480))
         #print(depth_img.shape)
         #print(np.nanmax(depth_img))
 
         # annotations_pose
         cam_pose = np.eye((4))
-        cam_pose[:3, 3] = mar_source[idx][1:4]
+        cam_pose[:3, 3] = mar_source[idx][1:4]# * 10.0
         quat = np.zeros((4))
         quat[1:] = mar_source[idx][4:-1]
         quat[0] = mar_source[idx][-1]
         cam_pose[:3, :3] = tf3d.quaternions.quat2mat(quat)
 
+        tDbox = cam_pose[:3, :3].dot(tdbox_ori.T).T
+        tDbox = tDbox + np.repeat(cam_pose[:3, 3][np.newaxis, :], 8, axis=0)
+        box3D = toPix_array(tDbox)
+        box3D = np.reshape(box3D, (16))
+        #box3D = box3D.tolist()
+        box3D = box3D.astype(np.uint16)
+
         #marker_pose = cam_pose
-        cam_pose = np.linalg.inv(cam_pose)
+        #cam_pose = np.linalg.inv(cam_pose)
         #marker_pose = cam_pose
 
         #traquat = np.zeros((7))
@@ -74,13 +107,48 @@ if __name__ == "__main__":
         #marker_pose = np.array(ann_source[idx][1:]).reshape(4,4).T
         #marker_pose = np.linalg.inv(marker_pose)
         #print(marker_pose)
-        #traquat = np.zeros((7))
-        #traquat[:3] = marker_pose[:3, 3]
-        #traquat[3:] = tf3d.quaternions.mat2quat(marker_pose[:3, :3])
 
-        #draw_axis(depth_img, traquat)
-        #cv2.imwrite("/home/stefan/LORF_viz/depth_" + str(idx) + ".png", (depth_img * (255/np.nanmax(depth_img))).astype(np.uint8))
-        #cv2.imwrite("/home/stefan/LORF_viz/depth_" + str(idx) + ".png", depth_img)
+        traquat = np.zeros((7))
+        traquat[:3] = cam_pose[:3, 3]
+        traquat[3:] = tf3d.quaternions.mat2quat(cam_pose[:3, :3])
+
+        colR = 250
+        colG = 25
+        colB = 175
+        '''
+        #rot_lie = [[0.0, pose[3], pose[4]], [-pose[3], 0.0, pose[5]], [-pose[4], -pose[5], 0.0]]
+        #ssm =np.asarray(rot_lie, dtype=np.float32)
+        #map = geometry.rotations.map_hat(ssm)
+        #quat = tf3d.euler.euler2quat(pose2D[3], pose2D[4], pose2D[5])
+        #quat = quat / np.linalg.norm(quat)
+        pose2D = np.concatenate([postra[i], pose2D[3:]])
+        #print('x: ', (pose[0]-bb[2])/(bb[4]-bb[2]))
+        #print('y: ', (pose[1] - bb[1]) / (bb[3] - bb[1]))
+        #print(pose[0:2], bb[1:])
+
+        #cv2.circle(img, (int(pose[0]), int(pose[1])), 5, (0, 255, 0), 3)
+        draw_axis(aug_xyz, pose2D)
+        '''
+        pose = box3D
+
+        img = cv2.line(img, tuple(pose[0:2].ravel()), tuple(pose[2:4].ravel()), (130, 245, 13), 2)
+        img = cv2.line(img, tuple(pose[2:4].ravel()), tuple(pose[4:6].ravel()), (50, 112, 220), 2)
+        img = cv2.line(img, tuple(pose[4:6].ravel()), tuple(pose[6:8].ravel()), (50, 112, 220), 2)
+        img = cv2.line(img, tuple(pose[6:8].ravel()), tuple(pose[0:2].ravel()), (50, 112, 220), 2)
+        img = cv2.line(img, tuple(pose[0:2].ravel()), tuple(pose[8:10].ravel()), (colR, colG, colB), 2)
+        img = cv2.line(img, tuple(pose[2:4].ravel()), tuple(pose[10:12].ravel()), (colR, colG, colB), 2)
+        img = cv2.line(img, tuple(pose[4:6].ravel()), tuple(pose[12:14].ravel()), (colR, colG, colB), 2)
+        img = cv2.line(img, tuple(pose[6:8].ravel()), tuple(pose[14:16].ravel()), (colR, colG, colB), 2)
+        img = cv2.line(img, tuple(pose[8:10].ravel()), tuple(pose[10:12].ravel()), (colR, colG, colB), 2)
+        img = cv2.line(img, tuple(pose[10:12].ravel()), tuple(pose[12:14].ravel()), (colR, colG, colB), 2)
+        img = cv2.line(img, tuple(pose[12:14].ravel()), tuple(pose[14:16].ravel()), (colR, colG, colB), 2)
+        img = cv2.line(img, tuple(pose[14:16].ravel()), tuple(pose[8:10].ravel()), (colR, colG, colB), 2)
+
+
+        draw_axis(img, traquat)
+        # cv2.imwrite("/home/stefan/LORF_viz/depth_" + str(idx) + ".png", (depth_img * (255/np.nanmax(depth_img))).astype(np.uint8))
+        cv2.imwrite("/home/stefan/LORF_viz/depth_" + str(idx) + ".png", img)
+
         #cam_pose = np.linalg.inv(ann_source[idx, 1:].reshape(4,4).T)
         #cam_pose = ann_source[idx, 1:].reshape(4, 4).T
         #cam_pose = np.eye((4))
@@ -88,7 +156,7 @@ if __name__ == "__main__":
         #print(mar_source[idx])
         #cam_pose[:3, 3] = mar_source[idx][1:4]
         #cam_pose[:3, :3] = tf3d.quaternions.quat2mat(mar_source[idx][4:])
-        print('cam_pose: ', cam_pose[:3, 3])
+        print(idx, 'cam_pose: ', cam_pose[:3, 3])
         cam_pose = cam_pose.tolist()
         current_view = {"file_path": img_path,
                         "rotation" : 0.0,
